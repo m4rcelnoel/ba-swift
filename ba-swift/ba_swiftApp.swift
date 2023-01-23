9046102f01f49f07f4e3101b5ba5c07ad1648150
Marcel-Gruber <102165699+Marcel-Gruber@users.noreply.github.com>
2023-01-23 10:19:22 +0100

    Initial Commit

---
+++ ba-swift/ba_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ba_swiftApp.swift
+//  ba-swift
+//
+//  Created by Marcel Gruber on 23.01.23.
+//
+
+import SwiftUI
+
+@main
+struct ba_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
